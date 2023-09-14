93077af336732331878401be7b96a3a49a0cea5e
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2023-09-14 05:46:47 +0200

    Register

--- controllers/authController.go
+++ controllers/authController.go
@@ -1,7 +1,28 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/JoseHurtadoBaeza/React-Golang-JWT-Authentication-freeCodeCamp/database"
+	"github.com/JoseHurtadoBaeza/React-Golang-JWT-Authentication-freeCodeCamp/models"
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+)
 
-func Hello(c *fiber.Ctx) error {
-	return c.SendString("Hello, World!")
+func Register(c *fiber.Ctx) error {
+	var data map[string]string // To get the data from the post request
+
+	if err := c.BodyParser(&data); err != nil { // To parse the data
+		return err
+	}
+
+	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14) // We hash the password using bcrypt package
+
+	user := models.User{
+		Name:     data["name"],
+		Email:    data["email"],
+		Password: password,
+	}
+
+	database.DB.Create(&user) // Insert the user in the database
+
+	return c.JSON(user)
 }
--- database/connection.go
+++ database/connection.go
@@ -1,16 +1,23 @@
 package database
 
 import (
+	"github.com/JoseHurtadoBaeza/React-Golang-JWT-Authentication-freeCodeCamp/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
 func Connect() {
 
-	_, err := gorm.Open(mysql.Open("root:1234@/go_auth"), &gorm.Config{})
+	connection, err := gorm.Open(mysql.Open("root:1234@/go_auth"), &gorm.Config{})
 
 	if err != nil {
 		panic("Could not connect to the database")
 	}
+
+	DB = connection
+
+	connection.AutoMigrate(&models.User{})
 
 }
